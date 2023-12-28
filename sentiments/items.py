36f2e4b5c4b46169034c5681629ed7407fb18242
mtwesley <mlen.too.wesley@gmail.com>
2023-12-28 15:14:35 -0500

    adding publisher item

--- sentiments/items.py
+++ sentiments/items.py
@@ -10,3 +10,17 @@ class SentimentsItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class PublisherItem(scrapy.Item):
+    identifier = scrapy.Field()
+    publisher = scrapy.Field()
+    date = scrapy.Field()
+    headline = scrapy.Field()
+    authors = scrapy.Field()
+    languages = scrapy.Field()
+    text = scrapy.Field()
+    summary = scrapy.Field()
+    keywords = scrapy.Field()
+    city = scrapy.Field()
+    country = scrapy.Field()
