0a7980d334e28a26d69aa8019b61a7e498adde25
mtwesley <mlen.too.wesley@gmail.com>
2023-12-28 20:47:24 -0500

    add mongodb pipeline

--- sentiments/pipelines.py
+++ sentiments/pipelines.py
@@ -5,9 +5,31 @@
 
 
 # useful for handling different item types with a single interface
+import pymongo
+import os
+
 from itemadapter import ItemAdapter
+from dotenv import load_dotenv
 
 
 class SentimentsPipeline:
     def process_item(self, item, spider):
+        return item
+
+
+class MongoPipeline(object):
+    collection_name = "scrapy_items"  # Collection name
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(os.getenv("MONGO_URI"))
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        collections = {
+            "publisher": "publishers",
+            "article": "articles",
+        }
+        self.db[collections[spider.name]].insert_one(dict(item))
         return item
